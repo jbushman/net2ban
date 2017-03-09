d52b5bcbe0ed465f91a70368cbb8256e73854a7f
Jacob Matthew Bushman <jacob.matthew.bushman@endurance.com>
2017-03-09 15:32:28 -0700

    removed logging

--- net2ban/net2ban.py
+++ net2ban/net2ban.py
@@ -4,12 +4,6 @@
 #
 
 import pika
-import logging
-
-logging.basicConfig(filename='/var/log/net2ban.log', level=logging.INFO)
-LOG_FORMAT = ('%(levelname) -10s %(asctime)s %(name) -30s %(funcName) '
-              '-35s %(lineno) -5d: %(message)s')
-LOGGER = logging.getLogger(__name__)
 
 
 class Consumer():
@@ -41,18 +35,15 @@ class Consumer():
               ssl=bool(self.ssl))
 
     def connect(self):
-        LOGGER.info('Connecting to %s', self._params)
         return pika.SelectConnection(self._params,
                                      self.on_connection_open,
                                      stop_ioloop_on_close=False)
 
     def on_connection_open(self, unused_connection):
-        LOGGER.info('Connection opened')
         self.add_on_connection_close_callback()
         self.open_channel()
 
     def add_on_connection_close_callback(self):
-        LOGGER.info('Adding connection close callback')
         self._connection.add_on_close_callback(self.on_connection_closed)
 
     def on_connection_closed(self, connection, reply_code, reply_text):
@@ -60,8 +51,6 @@ class Consumer():
         if self._closing:
             self._connection.ioloop.stop()
         else:
-            LOGGER.warning('Connection closed, reopening in 5 seconds: (%s) %s',
-                           reply_code, reply_text)
             self._connection.add_timeout(5, self.reconnect)
 
     def reconnect(self):
@@ -77,84 +66,63 @@ class Consumer():
             self._connection.ioloop.start()
 
     def open_channel(self):
-        LOGGER.info('Creating a new channel')
         self._connection.channel(on_open_callback=self.on_channel_open)
 
     def on_channel_open(self, channel):
-        LOGGER.info('Channel opened')
         self._channel = channel
         self.add_on_channel_close_callback()
         self.setup_exchange(self.exchange)
 
     def add_on_channel_close_callback(self):
-        LOGGER.info('Adding channel close callback')
         self._channel.add_on_close_callback(self.on_channel_closed)
 
     def on_channel_closed(self, channel, reply_code, reply_text):
-        LOGGER.warning('Channel %i was closed: (%s) %s',
-                       channel, reply_code, reply_text)
         self._connection.close()
 
     def setup_exchange(self, exchange_name):
-        LOGGER.info('Declaring exchange_type %s', self.exchange_type)
         self._channel.exchange_declare(self.on_exchange_declareok,
                                        exchange_name,
                                        self.exchange_type)
 
     def on_exchange_declareok(self, unused_frame):
-        LOGGER.info('Exchange declared')
         self.setup_queue(self.queue)
 
     def setup_queue(self, queue_name):
-        LOGGER.info('Declaring queue %s', queue_name)
         self._channel.queue_declare(self.on_queue_declareok, queue_name)
 
     def on_queue_declareok(self, method_frame):
-        LOGGER.info('Binding %s to %s with %s',
-                    self.exchange, self.queue, self.routing_key)
         self._channel.queue_bind(self.on_bindok, self.queue,
                                  self.exchange, self.routing_key)
 
     def on_bindok(self, unused_frame):
-        LOGGER.info('Queue bound')
         self.start_consuming()
 
     def start_consuming(self):
-        LOGGER.info('Issuing consumer related RPC commands')
         self.add_on_cancel_callback()
         self._consumer_tag = self._channel.basic_consume(self.on_message,
                                                          self.queue)
 
     def add_on_cancel_callback(self):
-        LOGGER.info('Adding consumer cancellation callback')
         self._channel.add_on_cancel_callback(self.on_consumer_cancelled)
 
     def on_consumer_cancelled(self, method_frame):
-        LOGGER.info('Consumer was cancelled remotely, shutting down: %r',
-                    method_frame)
         if self._channel:
             self._channel.close()
 
     def on_message(self, unused_channel, basic_deliver, properties, body):
-        LOGGER.info('Received message # %s from %s: %s',
-                    basic_deliver.delivery_tag, properties.app_id, body)
         self.acknowledge_message(basic_deliver.delivery_tag)
 
     def acknowledge_message(self, delivery_tag):
-        LOGGER.info('Acknowledging message %s', delivery_tag)
         self._channel.basic_ack(delivery_tag)
 
     def stop_consuming(self):
         if self._channel:
-            LOGGER.info('Sending a Basic.Cancel RPC command to RabbitMQ')
             self._channel.basic_cancel(self.on_cancelok, self._consumer_tag)
 
     def on_cancelok(self, unused_frame):
-        LOGGER.info('RabbitMQ acknowledged the cancellation of the consumer')
         self.close_channel()
 
     def close_channel(self):
-        LOGGER.info('Closing the channel')
         self._channel.close()
 
     def run(self):
@@ -162,12 +130,9 @@ class Consumer():
         self._connection.ioloop.start()
 
     def stop(self):
-        LOGGER.info('Stopping')
         self._closing = True
         self.stop_consuming()
         self._connection.ioloop.start()
-        LOGGER.info('Stopped')
 
     def close_connection(self):
-        LOGGER.info('Closing connection')
         self._connection.close()
